ee807a4955a9dff011ad91ad09adab754c100e68
ToMMaaY <thuynh629@gmail.com>
2019-01-22 10:43:19 -0800

    Hooked up the login and logout endpoints

--- api/server.js
+++ api/server.js
@@ -3,13 +3,27 @@ const helmet = require("helmet");
 const knex = require("knex");
 const knexConfig = require("../knexfile");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
 const server = express();
 
 const db = knex(knexConfig.development);
 
+const sessionConfig = {
+  name: "ayo", // default is sid, but this will show you are using the library
+  secret: "kjdfhasgyrahbbvkjsdbfiaasbfyabf",
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false // only send the cookie over https, use true in production
+  },
+  httpOnly: true, // js can't touch this cookie
+  resave: false, // read about this
+  saveUninitialized: false // read about this
+};
+
 server.use(helmet());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.get("/", (req, res) => {
   res.send("Sanity check.");
@@ -28,11 +42,6 @@ server.post("/users/register", async (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-server.get("/users", async (req, res) => {
-  const users = await db("users");
-  res.json(users);
-});
-
 server.post("/login", (req, res) => {
   const creds = req.body;
 
@@ -41,12 +50,42 @@ server.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
-        res.status(200).json({ message: `Weclome ${user.name}` });
+        req.session.user = user;
+        res.status(200).json({ message: `Welcome ${user.name}` });
       } else {
-        res.status(401).json({ message: "Cannot login." });
+        res.status(401).json({ message: "You shall not pass." });
       }
     })
     .catch(err => res.status(500).json(err));
+});
+
+function protected(req, res, next) {
+  // if the user us logged in, we call next()
+  if (req.session && req.session.user) {
+    console.log(req.session);
+    next();
+  } else {
+    res.status(401).json({ message: "You shall not pass, not authenticated." });
+  }
+}
+
+server.get("/users", protected, async (req, res) => {
+  const users = await db("users");
+  res.json(users);
+});
+
+server.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).send("You can never leave.");
+      } else {
+        res.status(200).send("Bye bye");
+      }
+    });
+  } else {
+    res.json({ message: "Already logged out." });
+  }
 });
 
 module.exports = server;
